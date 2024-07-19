2365db2a3e6840fad642b6b08239452bf36dfe57
Ilya <ilyailyailya.ilya@yandex.ru>
2024-07-19 17:31:50 +0300

    19.07.2024

--- single-catalog.php
+++ single-catalog.php
@@ -156,7 +156,100 @@ if(has_post_parent()) :
         while ( have_rows('section') ) : the_row();
 
             if( get_row_layout() == 'auto' ):
-                echo get_template_part( 'template-parts/auto' );
+
+    ?>
+    
+        <section class="auto">
+            <h3 class="h2">
+                <?php if(get_sub_field('title'))  {the_sub_field('title');} else {the_field('auto-title', 13);} ?>
+            </h3>
+            <p class="regular-l">
+                <?php if(get_sub_field('desc'))  {the_sub_field('desc');} else {the_field('auto-desc', 13);} ?>
+            </p>
+            <div class="auto-list swiper">
+                <div class="swiper-wrapper">
+                    <?php
+                    
+                        $loop = new WP_Query( array(
+                            'post_type' => 'cars',
+                            'posts_per_page' => -1,
+                            'tax_query' => array(
+                                array(
+                                    'taxonomy' => 'cars-cat',
+                                    'field' => 'slug',
+                                    'terms' => get_the_title(),
+                                ),
+                            ),
+                        ));
+
+                        if(!$loop->have_posts()) {
+                            $loop = new WP_Query( array(
+                                'post_type' => 'cars',
+                                'posts_per_page' => 10,
+                                'orderby'        => 'rand',
+                            ));
+                        }
+
+                        while($loop->have_posts()) : $loop->the_post();
+
+                        $card = get_field('card');
+                    ?>
+                            <div class="swiper-slide auto-list-item auto-list-item_service">
+                                <a href="<?php the_permalink(); ?>" class="auto-list-item-img">
+                                    <?php if($card['value']) : ?>
+                                        <p class="medium-s value"><?php echo $card['value']; ?></p>
+                                    <?php endif; ?>
+                                    <?php if($card['country']['name']) : ?>
+                                        <p class="medium-s country">
+                                            <img src="<?php echo $card['country']['img']['url']; ?>" alt="country">
+                                            <?php echo $card['country']['name']; ?>
+                                        </p>
+                                    <?php endif; ?>
+                                    <img src="<?php echo $card['img']['url']; ?>" alt="photo">
+                                </a> 
+                                <div class="auto-list-item-inner">
+                                    <p class="h3"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
+                                    <p class="medium-s date"><?php echo $card['date']; ?></p>
+                                    <?php if($card['service']) : ?>
+                                        <p class="medium-s service"><?php echo $card['service']; ?></p>
+                                    <?php endif; ?>
+                                    <div class="auto-list-item-list">
+                                        <?php 
+                                            foreach($card['list'] as $item) :
+                                        ?>
+                                            <div class="auto-list-item-list-item">
+                                                <p class="regular-m">
+                                                    <span><?php echo $item['name']; ?></span>
+                                                    <?php echo $item['value']; ?>
+                                                </p>
+                                            </div>
+                                        <?php endforeach; ?>
+                                    </div>
+                                    <p class="medium-m"><?php echo $card['check']; ?></p>
+                                    <p class="regular-s">Стоимость автомобиля</p>
+                                    <p class="h3 price"><?php echo $card['price']; ?></p>
+                                    <div class="auto-list-item-btns">
+                                        <div class="primary-btn semibold-s call-btn" data-text="<?php the_title(); ?>">Хочу такой же</div>
+                                        <a href="<?php the_permalink(); ?>" class="secondary-btn semibold-s">Смотреть отчет</a>
+                                    </div>
+                                </div>
+                            </div>
+
+                    <?php 
+                        endwhile;
+                        wp_reset_postdata();
+                    ?>
+                </div>
+            </div>
+            <a href="/cars" class="section-link">
+                <p class="semibold-m">Смотреть все подобранные авто</p>
+                <div class="section-link-icon">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/img/dropdown.svg" alt="icon" class="svg">
+                </div>
+            </a>
+        </section>
+
+    <?php 
 
             endif;
 
